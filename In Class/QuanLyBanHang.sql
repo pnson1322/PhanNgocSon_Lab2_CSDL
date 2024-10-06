feb93ebc2130b14d92ec05ab67a8f38a86faa51f
pnson1322 <pnson22@gmail.com>
2024-10-06 15:11:10 +0700

    Add files via upload

---
+++ In Class/QuanLyBanHang.sql
@@ -0,0 +1,278 @@
+﻿-- Tạo database quản lí bán hàng
+CREATE DATABASE QUANLIBANHANG 
+
+USE QUANLIBANHANG
+
+-- Tạo các bảng của database
+CREATE TABLE KHACHHANG(
+	MAKH char(4) PRIMARY KEY,
+	HOTEN varchar(40),
+	DIACHI varchar(50),
+	SDT varchar(20),
+	NGSINH smalldatetime,
+	DOANHSO money,
+	NGDK smalldatetime,
+);
+
+CREATE TABLE NHANVIEN(
+	MANV char(4) PRIMARY KEY,
+	HOTEN varchar(40),
+	NGVL smalldatetime,
+	SDT varchar(20),
+);
+
+CREATE TABLE SANPHAM(
+	MASP char(4) PRIMARY KEY,
+	TENSP varchar(40),
+	DVT varchar(20),
+	NUOCSX varchar(20),
+	GIA money,
+);
+
+CREATE TABLE HOADON(
+	SOHD int PRIMARY KEY,
+	NGHD smalldatetime,
+	MAKH char(4) REFERENCES KHACHHANG(MAKH),
+	MANV char(4) REFERENCES NHANVIEN(MANV),
+	TRIGIA money,
+);
+
+CREATE TABLE CTHD(
+	SOHD int REFERENCES HOADON(SOHD),
+	MASP char(4) REFERENCES SANPHAM(MASP),
+	SL int CHECK (SL >= 1),
+	PRIMARY KEY (SOHD, MASP),
+);
+
+
+
+-- I.
+-- 2.	Thêm vào thuộc tính GHICHU có kiểu dữ liệu varchar(20) cho quan hệ SANPHAM.
+ALTER TABLE SANPHAM
+ADD GHICHU VARCHAR(20)
+
+-- 3.	Thêm vào thuộc tính LOAIKH có kiểu dữ liệu là tinyint cho quan hệ KHACHHANG.
+ALTER TABLE KHACHHANG
+ADD LOAIKH TINYINT
+
+-- 4.	Sửa kiểu dữ liệu của thuộc tính GHICHU trong quan hệ SANPHAM thành varchar(100).
+ALTER TABLE SANPHAM
+ALTER COLUMN GHICHU VARCHAR(100)
+
+-- 5.	Xóa thuộc tính GHICHU trong quan hệ SANPHAM.
+ALTER TABLE SANPHAM
+DROP COLUMN GHICHU
+
+-- 6.	Làm thế nào để thuộc tính LOAIKH trong quan hệ KHACHHANG có thể lưu các giá trị là: “Vang lai”, “Thuong xuyen”, “Vip”, …
+ALTER TABLE KHACHHANG
+ALTER COLUMN LOAIKH VARCHAR(20)
+
+-- 7.	Đơn vị tính của sản phẩm chỉ có thể là (“cay”,”hop”,”cai”,”quyen”,”chuc”)
+ALTER TABLE SANPHAM ADD 
+CONSTRAINT CK_DVT CHECK(DVT in ('cay', 'hop', 'cai', 'quyen', 'chuc'))
+
+-- 8.	Giá bán của sản phẩm từ 500 đồng trở lên.
+ALTER TABLE SANPHAM ADD
+CONSTRAINT CK_GIA CHECK (GIA > 500)
+
+-- 9.	Mỗi lần mua hàng, khách hàng phải mua ít nhất 1 sản phẩm. Đã thêm sẵn trên quan hệ.
+
+-- 10.	Ngày khách hàng đăng ký là khách hàng thành viên phải lớn hơn ngày sinh của người đó.
+ALTER TABLE KHACHHANG ADD 
+CONSTRAINT CK_NGDK CHECK(NGDK > NGSINH)
+
+
+
+-- II. 
+-- 1. Nhập dữ liệu cho cơ sở dữ liệu
+INSERT INTO SANPHAM (MASP, TENSP, DVT, NUOCSX, GIA)
+VALUES
+('BC01', 'But chi', 'cay', 'Singapore', 3000),
+('BC02', 'But chi', 'cay', 'Singapore', 5000),
+('BC03', 'But chi', 'cay', 'Viet Nam', 3500),
+('BC04', 'But chi', 'hop', 'Viet Nam', 30000),
+('BB01', 'But bi', 'cay', 'Viet Nam', 5000),
+('BB02', 'But bi', 'cay', 'Trung Quoc', 7000),
+('BB03', 'But bi', 'hop', 'Thai Lan', 100000),
+('TV01', 'Tap 100 giay mong', 'quyen', 'Trung Quoc', 2500),
+('TV02', 'Tap 200 giay mong', 'quyen', 'Trung Quoc', 4500),
+('TV03', 'Tap 100 giay tot', 'quyen', 'Viet Nam', 3000),
+('TV04', 'Tap 200 giay tot', 'quyen', 'Viet Nam', 5500),
+('TV05', 'Tap 100 trang', 'chuc', 'Viet Nam', 23000),
+('TV06', 'Tap 200 trang', 'chuc', 'Viet Nam', 53000),
+('TV07', 'Tap 100 trang', 'chuc', 'Trung Quoc', 34000),
+('ST01', 'So tay 500 trang', 'quyen', 'Trung Quoc', 40000),
+('ST02', 'So tay loai 1', 'quyen', 'Viet Nam', 55000),
+('ST03', 'So tay loai 2', 'quyen', 'Viet Nam', 51000),
+('ST04', 'So tay', 'quyen', 'Thai Lan', 55000),
+('ST05', 'So tay mong', 'quyen', 'Thai Lan', 20000),
+('ST06', 'Phan viet bang', 'hop', 'Viet Nam', 5000),
+('ST07', 'Phan khong bui', 'hop', 'Viet Nam', 7000),
+('ST08', 'Bong bang', 'cai', 'Viet Nam', 1000),
+('ST09', 'But long', 'cay', 'Viet Nam', 5000),
+('ST10', 'But long', 'cay', 'Trung Quoc', 7000);
+
+INSERT INTO NHANVIEN (MANV, HOTEN, SDT, NGVL) 
+VALUES 
+('NV01', 'Nguyen Nhu Nhut', '0927345678', '2006-04-13'),
+('NV02', 'Le Thi Phi Yen', '0987567390', '2006-04-21'),
+('NV03', 'Nguyen Van B', '0997047382', '2006-04-27'),
+('NV04', 'Ngo Thanh Tuan', '0913758498', '2006-06-24'),
+('NV05', 'Nguyen Thi Truc Thanh', '0918590387', '2006-07-20');
+
+INSERT INTO KHACHHANG (MAKH, HOTEN, DIACHI, SDT, NGSINH, DOANHSO, NGDK)
+VALUES
+('KH01', 'Nguyen Van A', '731 Tran Hung Dao, Q5, TpHCM', '08823451', '1960-10-22', 13060000, '2006-07-22'),
+('KH02', 'Tran Ngoc Han', '23/5 Nguyen Trai, Q5, TpHCM', '0908256478', '1974-04-03', 280000, '2006-07-30'),
+('KH03', 'Tran Ngoc Linh', '45 Nguyen Canh Chan, Q1, TpHCM', '0938776266', '1980-06-12', 3860000, '2006-08-05'),
+('KH04', 'Tran Minh Long', '50/34 Le Dai Hanh, Q10, TpHCM', '0917325476', '1965-03-09', 250000, '2006-10-02'),
+('KH05', 'Le Nhat Minh', '34 Truong Dinh, Q3, TpHCM', '08246108', '1950-03-10', 21000, '2006-10-28'),
+('KH06', 'Le Hoai Thuong', '227 Nguyen Van Cu, Q5, TpHCM', '08631738', '1981-12-31', 915000, '2006-11-24'),
+('KH07', 'Nguyen Van Tam', '32/3 Tran Binh Trong, Q5, TpHCM', '0916783565', '1971-04-06', 12500, '2006-12-01'),
+('KH08', 'Phan Thi Thanh', '45/2 An Duong Vuong, Q5, TpHCM', '0938435756', '1971-01-10', 365000, '2006-12-13'),
+('KH09', 'Le Ha Vinh', '873 Le Hong Phong, Q5, TpHCM', '08654763', '1979-09-03', 70000, '2007-01-14'),
+('KH10', 'Ha Duy Lap', '34/34B Nguyen Trai, Q1, TpHCM', '08768904', '1983-05-02', 67500, '2007-01-16');
+
+INSERT INTO HOADON (SOHD, NGHD, MAKH, MANV, TRIGIA)
+VALUES
+(1001, '2006-07-23', 'KH01', 'NV01', 320000),
+(1002, '2006-08-12', 'KH01', 'NV02', 840000),
+(1003, '2006-08-23', 'KH02', 'NV01', 100000),
+(1004, '2006-09-01', 'KH02', 'NV01', 180000),
+(1005, '2006-10-20', 'KH01', 'NV02', 3800000),
+(1006, '2006-10-16', 'KH01', 'NV03', 2430000),
+(1007, '2006-10-28', 'KH03', 'NV03', 510000),
+(1008, '2006-10-28', 'KH01', 'NV03', 440000),
+(1009, '2006-10-28', 'KH03', 'NV04', 200000),
+(1010, '2006-11-01', 'KH01', 'NV01', 5200000),
+(1011, '2006-11-04', 'KH04', 'NV03', 250000),
+(1012, '2006-11-30', 'KH05', 'NV03', 21000),
+(1013, '2006-12-12', 'KH06', 'NV01', 5000),
+(1014, '2006-12-31', 'KH03', 'NV02', 3150000),
+(1015, '2007-01-01', 'KH06', 'NV01', 910000),
+(1016, '2007-01-01', 'KH07', 'NV02', 12500),
+(1017, '2007-01-02', 'KH08', 'NV03', 35000),
+(1018, '2007-01-13', 'KH08', 'NV03', 330000),
+(1019, '2007-01-13', 'KH01', 'NV03', 30000),
+(1020, '2007-01-14', 'KH09', 'NV04', 70000),
+(1021, '2007-01-16', 'KH10', 'NV03', 67500),
+(1022, '2007-01-16', NULL, 'NV03', 7000),
+(1023, '2007-01-17', NULL, 'NV01', 330000);
+
+INSERT INTO CTHD (SOHD, MASP, SL)
+VALUES
+(1001, 'TV02', 10),
+(1001, 'ST01', 5),
+(1001, 'BC01', 5),
+(1001, 'BC02', 10),
+(1001, 'ST08', 10),
+(1002, 'BC04', 20),
+(1002, 'BB01', 20),
+(1002, 'BB02', 20),
+(1003, 'BB03', 10),
+(1004, 'TV01', 20),
+(1004, 'TV02', 10),
+(1004, 'TV03', 10),
+(1004, 'TV04', 10),
+(1005, 'TV05', 50),
+(1005, 'TV06', 50),
+(1006, 'TV07', 20),
+(1006, 'ST01', 30),
+(1006, 'ST02', 10),
+(1007, 'ST03', 10),
+(1008, 'ST04', 8),
+(1009, 'ST05', 10),
+(1010, 'TV07', 50),
+(1010, 'ST07', 50),
+(1010, 'ST08', 100),
+(1010, 'ST04', 50),
+(1010, 'TV03', 100),
+(1011, 'ST06', 50),
+(1012, 'ST07', 3),
+(1013, 'ST08', 5),
+(1014, 'BC02', 80),
+(1014, 'BB02', 100),
+(1014, 'BC04', 60),
+(1014, 'BB01', 50),
+(1015, 'BB02', 30),
+(1015, 'BB03', 7),
+(1016, 'TV01', 5),
+(1017, 'TV02', 1),
+(1017, 'TV03', 1),
+(1017, 'TV04', 5),
+(1018, 'ST04', 6),
+(1019, 'ST05', 1),
+(1019, 'ST06', 2),
+(1020, 'ST07', 10),
+(1021, 'ST08', 5),
+(1021, 'TV01', 7),
+(1021, 'TV02', 10),
+(1022, 'ST07', 1),
+(1023, 'ST04', 6);
+
+-- 2. Tạo quan hệ SANPHAM1 chứa toàn bộ dữ liệu của quan hệ SANPHAM. Tạo quan hệ KHACHHANG1 chứa toàn bộ dữ liệu của quan hệ KHACHHANG.
+SELECT * INTO SANPHAM1 FROM SANPHAM
+SELECT * INTO KHACHHANG1 FROM KHACHHANG
+
+-- 3. Cập nhật giá tăng 5% đối với những sản phẩm do “Thai Lan” sản xuất (cho quan hệ SANPHAM1)
+UPDATE SANPHAM1
+SET GIA = GIA * 1.05
+WHERE NUOCSX = 'Thai Lan'
+
+-- 4. Cập nhật giá giảm 5% đối với những sản phẩm do “Trung Quoc” sản xuất có giá từ 10.000 trở xuống (cho quan hệ SANPHAM1).
+UPDATE SANPHAM1
+SET GIA = 0.95
+WHERE NUOCSX = 'Trung Quoc' AND GIA <= 10000
+
+-- 5. Cập nhật giá trị LOAIKH là “Vip” đối với những khách hàng đăng ký thành viên trước ngày 1/1/2007 có doanh số từ 10.000.000 trở lên hoặc khách hàng đăng ký thành viên từ 1/1/2007 trở về sau có doanh số từ 2.000.000 trở lên (cho quan hệ KHACHHANG1).
+UPDATE KHACHHANG1
+SET LOAIKH = 'Vip'
+WHERE (YEAR(NGDK) < 2007 AND DOANHSO >= 10000000) OR (YEAR(NGDK) >= 2007 AND DOANHSO >= 2000000)
+
+
+
+-- III.
+-- 1. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất.
+SELECT MASP, TENSP FROM SANPHAM
+WHERE NUOCSX = 'Trung Quoc'
+
+-- 2. In ra danh sách các sản phẩm (MASP, TENSP) có đơn vị tính là “cay”, ”quyen”.
+SELECT MASP, TENSP FROM SANPHAM
+WHERE DVT IN ('cay', 'quyen')
+
+-- 3. In ra danh sách các sản phẩm (MASP,TENSP) có mã sản phẩm bắt đầu là “B” và kết thúc là “01”.
+SELECT MASP, TENSP FROM SANPHAM
+WHERE MASP LIKE 'B%01'
+
+-- 4. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quốc” sản xuất có giá từ 30.000 đến 40.000.
+SELECT MASP, TENSP FROM SANPHAM
+WHERE (NUOCSX = 'Trung Quoc') AND (GIA BETWEEN 30000 AND 40000)
+
+-- 5. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” hoặc “Thai Lan” sản xuất có giá từ 30.000 đến 40.000.
+SELECT MASP, TENSP FROM SANPHAM
+WHERE (NUOCSX IN ('Trung Quoc', 'Thai Lan')) AND (GIA BETWEEN 30000 AND 40000)
+
+-- 6. In ra các số hóa đơn, trị giá hóa đơn bán ra trong ngày 1/1/2007 và ngày 2/1/2007.
+SELECT SOHD, TRIGIA FROM HOADON
+WHERE NGHD BETWEEN '2007-1-1' AND '2007-1-2'
+
+-- 7. In ra các số hóa đơn, trị giá hóa đơn trong tháng 1/2007, sắp xếp theo ngày (tăng dần) và trị giá của hóa đơn (giảm dần).
+SELECT SOHD, TRIGIA FROM HOADON
+WHERE MONTH(NGHD) = 1 AND YEAR(NGHD) = 2007
+ORDER BY DAY(NGHD) ASC, TRIGIA DESC
+
+-- 8. In ra danh sách các khách hàng (MAKH, HOTEN) đã mua hàng trong ngày 1/1/2007.
+SELECT HOADON.MAKH, HOTEN FROM KHACHHANG, HOADON
+WHERE KHACHHANG.MAKH = HOADON.MAKH AND NGHD = '2007-1-1'
+
+-- 9. In ra số hóa đơn, trị giá các hóa đơn do nhân viên có tên “Nguyen Van B” lập trong ngày 28/10/2006.
+SELECT SOHD, TRIGIA FROM HOADON, NHANVIEN
+WHERE HOADON.MANV = NHANVIEN.MANV AND HOTEN = 'Nguyen Van B' AND NGHD = '2006-10-28'
+
+-- 10. In ra danh sách các sản phẩm (MASP,TENSP) được khách hàng có tên “Nguyen Van A” mua trong tháng 10/2006.
+SELECT SANPHAM.MASP, TENSP FROM SANPHAM, KHACHHANG, HOADON, CTHD
+WHERE SANPHAM.MASP = CTHD.MASP AND CTHD.SOHD = HOADON.SOHD AND HOADON.MAKH = KHACHHANG.MAKH AND HOTEN = 'Nguyen Van A' AND MONTH(NGHD) = 10 AND YEAR(NGHD) = 2006
+
+-- 11. Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”.
+SELECT SOHD FROM CTHD
+WHERE MASP IN ('BB01', 'BB02')
---
+++ In Class/QuanLyGiaoVu.sql
@@ -0,0 +1,356 @@
+﻿-- Tạo database quản lí giáo vụ
+CREATE DATABASE QUANLIGIAOVU
+
+USE QUANLIGIAOVU
+
+-- Tạo các quan hệ trong database
+CREATE TABLE HOCVIEN(
+	 MAHV char(5) PRIMARY KEY,
+	 HO varchar(40),
+	 TEN varchar(10),
+	 NGSINH smalldatetime,
+	 GIOITINH varchar(3),
+	 NOISINH varchar(40),
+	 MALOP char(4),
+);
+
+ALTER TABLE HOCVIEN
+ALTER COLUMN MALOP CHAR (3)
+
+ALTER TABLE HOCVIEN ADD
+CONSTRAINT FK_MALOP FOREIGN KEY (MALOP) REFERENCES LOP(MALOP)
+
+CREATE TABLE KHOA(
+	MAKHOA varchar(4) PRIMARY KEY,
+	TENKHOA varchar(40),
+	NGTLAP smalldatetime,
+	TRGKHOA char(4),
+);
+
+CREATE TABLE MONHOC(
+	MAMH VARCHAR(10) PRIMARY KEY,
+	TENMH VARCHAR(40),
+	TCLT TINYINT,
+	TCTH TINYINT,
+	MAKHOA VARCHAR(4) REFERENCES KHOA(MAKHOA),
+);
+
+CREATE TABLE DIEUKIEN(
+	MAMH VARCHAR(10) REFERENCES MONHOC(MAMH),
+	MAMH_TRUOC VARCHAR(10) REFERENCES MONHOC(MAMH),
+	PRIMARY KEY (MAMH, MAMH_TRUOC),
+);
+
+CREATE TABLE GIAOVIEN(
+	MAGV CHAR(4) PRIMARY KEY,
+	HOTEN VARCHAR(40),
+	HOCVI VARCHAR(10),
+	HOCHAM VARCHAR(10),
+	GIOITINH VARCHAR(3),
+	NGSINH SMALLDATETIME,
+	NGVL SMALLDATETIME,
+	HESO NUMERIC(4,2),
+	MUCLUONG MONEY,
+	MAKHOA VARCHAR(4) REFERENCES KHOA(MAKHOA),
+);
+
+CREATE TABLE LOP(
+	MALOP CHAR(3) PRIMARY KEY,
+	TENLOP VARCHAR(40),
+	TRGLOP CHAR(5) REFERENCES HOCVIEN(MAHV),
+	SISO TINYINT,
+	MAGVCN CHAR(4) REFERENCES GIAOVIEN(MAGV),
+);
+
+CREATE TABLE GIANGDAY(
+	MALOP CHAR(3) REFERENCES LOP(MALOP),
+	MAMH VARCHAR(10) REFERENCES MONHOC(MAMH),
+	MAGV CHAR(4) REFERENCES GIAOVIEN(MAGV),
+	HOCKY TINYINT,
+	NAM SMALLINT,
+	TUNGAY SMALLDATETIME,
+	DENNGAY SMALLDATETIME,
+	PRIMARY KEY (MALOP, MAMH),
+);
+
+CREATE TABLE KETQUATHI(
+	MAMH VARCHAR(10) REFERENCES MONHOC(MAMH),
+	MAHV CHAR(5) REFERENCES HOCVIEN(MAHV),
+	LANTHI TINYINT,
+	NGTHI SMALLDATETIME,
+	DIEM NUMERIC(4,2),
+	KQUA VARCHAR(10),
+	PRIMARY KEY(MAHV, MAMH, LANTHI),
+);
+
+ALTER TABLE KHOA ADD
+CONSTRAINT FK_TGK_GV FOREIGN KEY (TRGKHOA) REFERENCES GIAOVIEN(MAGV)
+
+
+
+-- I.
+-- 3. Thuộc tính của GIOITINH chỉ có giá trị là "Nam" hoặc "Nữ".
+ALTER TABLE HOCVIEN ADD
+CONSTRAINT CK_GT_HV CHECK (GIOITINH IN ('Nam', 'Nu'))
+
+ALTER TABLE GIAOVIEN ADD
+CONSTRAINT CK_GT_GV CHECK (GIOITINH IN ('Nam', 'Nu'))
+
+-- 4. Điểm số của một lần thi có giá trị từ 0 đến 10 và cần lưu đến 2 số lẽ (VD: 6.22).
+ALTER TABLE KETQUATHI ADD 
+CONSTRAINT CK_DIEM CHECK (
+	DIEM BETWEEN 0 AND 10 AND
+	LEN(SUBSTRING(CAST(DIEM AS VARCHAR), CHARINDEX('.', DIEM) + 1, 1000)) >= 2
+)
+
+-- 5. Kết quả thi là “Dat” nếu điểm từ 5 đến 10  và “Khong dat” nếu điểm nhỏ hơn 5.
+ALTER TABLE KETQUATHI ADD 
+CONSTRAINT CK_KQUA CHECK (KQUA = IIF(DIEM BETWEEN 5 AND 10, 'Dat', 'Khong dat'))
+
+-- 6. Học viên thi một môn tối đa 3 lần.
+ALTER TABLE KETQUATHI ADD
+CONSTRAINT CK_SOLANTHI CHECK (LANTHI <= 3)
+
+-- 7. Học kì chỉ có giá trị từ 1 đến 3.
+ALTER TABLE GIANGDAY ADD
+CONSTRAINT CK_HOCKY CHECK (HOCKY BETWEEN 1 AND 3)
+
+-- 8. Học vị của giáo viên chỉ có thể là "CN", "KS", "ThS", "TS", "PTS".
+ALTER TABLE GIAOVIEN ADD
+CONSTRAINT CK_HOCVI CHECK (HOCVI IN ('CN', 'KS', 'ThS', 'TS', 'PTS'))
+
+
+
+-- Nhập dữ liệu cho cơ sở dữ liệu
+INSERT KHOA (MAKHOA, TENKHOA, NGTLAP, TRGKHOA) VALUES 
+(N'KHMT', N'Khoa hoc may tinh', CAST(N'2005-06-07T00:00:00.000' AS DateTime), NULL),
+(N'HTTT', N'He thong thong tin', CAST(N'2005-06-07T00:00:00.000' AS DateTime), NULL),
+(N'CNPM', N'Cong nghe phan mem', CAST(N'2005-06-07T00:00:00.000' AS DateTime), NULL),
+(N'MTT', N'Mang va truyen thong', CAST(N'2005-10-20T00:00:00.000' AS DateTime), NULL),
+(N'KTMT', N'Ky thuat may tinh', CAST(N'2005-12-20T00:00:00.000' AS DateTime), NULL);
+
+INSERT MONHOC (MAMH, TENMH, TCLT, TCTH, MAKHOA) VALUES
+(N'THDC', N'Tin hoc dai cuong', 4, 1, N'KHMT'),
+(N'CTRR', N'Cau truc roi rac', 5, 0, N'KHMT'),
+(N'CSDL', N'Co so du lieu', 3, 1, N'HTTT'),
+(N'CTDLGT', N'Cau truc du lieu va giai thuat', 3, 1, N'KHMT'),
+(N'PTTKTT', N'Phan tich thiet ke thuat toan', 3, 0, N'KHMT'),
+(N'DHMT', N'Do hoa may tinh', 3, 1, N'KHMT'),
+(N'KTMT', N'Kien truc may tinh', 3, 0, N'KTMT'),
+(N'TKCSDL', N'Thiet ke co so du lieu', 3, 1, N'HTTT'),
+(N'PTTKHTTT', N'Phan tich thiet ke he thong thong tin', 4, 1, N'HTTT'),
+(N'HDH', N'He dieu hanh', 4, 0, N'KTMT'),
+(N'NMCNPM', N'Nhap mon cong nghe phan mem', 3, 0, N'CNPM'),
+(N'LTCFW', N'Lap trinh C for win', 3, 1, N'CNPM'),
+(N'LTHDT', N'Lap trinh huong doi tuong', 3, 1, N'CNPM');
+
+INSERT DIEUKIEN (MAMH, MAMH_TRUOC) VALUES
+(N'CSDL', N'CTRR'),
+(N'CSDL', N'CTDLGT'),
+(N'CTDLGT', N'THDC'),
+(N'PTTKTT', N'THDC'),
+(N'PTTKTT', N'CTDLGT'),
+(N'DHMT', N'THDC'),
+(N'LTHDT', N'THDC'),
+(N'PTTKHTTT', N'CSDL');
+
+INSERT GIAOVIEN (MAGV, HOTEN, HOCVI, HOCHAM, GIOITINH, NGSINH, NGVL, HESO, MUCLUONG, MAKHOA) VALUES
+(N'GV01', N'Ho Thanh Son', N'PTS', N'GS', N'Nam', '1950-05-02', '2004-01-11', 5, 2250000, N'KHMT'),
+(N'GV02', N'Tran Tam Thanh', N'TS', N'PGS', N'Nam', '1965-12-17', '2004-04-20', 4.5, 2025000, N'HTTT'),
+(N'GV03', N'Do Nghiem Phung', N'TS', N'GS', N'Nu', '1950-08-01', '2004-09-23', 4, 1800000, N'CNPM'),
+(N'GV04', N'Tran Nam Son', N'TS', N'PGS', N'Nam', '1961-02-22', '2005-01-12', 4.5, 2025000, N'KTMT'),
+(N'GV05', N'Mai Thanh Danh', N'ThS', N'GV', N'Nam', '1958-03-12', '2005-01-12', 3, 1350000, N'HTTT'),
+(N'GV06', N'Tran Doan Hung', N'TS', N'GV', N'Nam', '1953-03-11', '2005-01-12', 4.5, 2025000, N'KHMT'),
+(N'GV07', N'Nguyen Minh Tien', N'ThS', N'GV', N'Nam', '1971-11-23', '2005-03-01', 4, 1800000, N'KHMT'),
+(N'GV08', N'Le Thi Tran', N'KS', N'Null', N'Nu', '1974-03-26', '2005-03-01', 1.69, 760500, N'KHMT'),
+(N'GV09', N'Nguyen To Lan', N'ThS', N'GV', N'Nu', '1966-12-31', '2005-03-01', 4, 1800000, N'HTTT'),
+(N'GV10', N'Le Tran Anh Loan', N'KS', N'Null', N'Nu', '1972-07-17', '2005-03-01', 1.86, 837000, N'CNPM'),
+(N'GV11', N'Ho Thanh Tung', N'CN', N'GV', N'Nam', '1980-01-12', '2005-05-15', 2.67, 1201500, N'MTT'),
+(N'GV12', N'Tran Van Anh', N'CN', N'Null', N'Nu', '1981-03-29', '2005-05-15', 1.69, 760500, N'CNPM'),
+(N'GV13', N'Nguyen Linh Dan', N'CN', N'Null', N'Nu', '1980-05-23', '2005-05-15', 1.69, 760500, N'KTMT'),
+(N'GV14', N'Truong Minh Chau', N'ThS', N'GV', N'Nu', '1976-11-30', '2005-05-15', 3, 1350000, N'MTT'),
+(N'GV15', N'Le Ha Thanh', N'ThS', N'GV', N'Nam', '1978-05-04', '2005-05-15', 3, 1350000, N'KHMT');
+
+INSERT LOP (MALOP, TENLOP, TRGLOP, SISO, MAGVCN) VALUES
+(N'K11', N'Lop 1 khoa 1', N'K1108', 11, N'GV07'),
+(N'K12', N'Lop 2 khoa 1', N'K1205', 12, N'GV09'),
+(N'K13', N'Lop 3 khoa 1', N'K1305', 12, N'GV14');
+
+INSERT HOCVIEN (MAHV, HO, TEN, NGSINH, GIOITINH, NOISINH, MALOP) VALUES
+(N'K1101', N'Nguyen Van', N'A', '1986-01-27', N'Nam', N'TpHCM', N'K11'),
+(N'K1102', N'Tran Ngoc', N'Han', '1986-03-14', N'Nu', N'Kien Giang', N'K11'),
+(N'K1103', N'Ha Duy', N'Lap', '1986-04-18', N'Nam', N'Nghe An', N'K11'),
+(N'K1104', N'Tran Ngoc', N'Linh', '1986-03-30', N'Nu', N'Tay Ninh', N'K11'),
+(N'K1105', N'Tran Minh', N'Long', '1986-02-27', N'Nam', N'TpHCM', N'K11'),
+(N'K1106', N'Le Nhat', N'Minh', '1986-01-24', N'Nam', N'TpHCM', N'K11'),
+(N'K1107', N'Nguyen Nhu', N'Nhut', '1986-01-27', N'Nam', N'Ha Noi', N'K11'),
+(N'K1108', N'Nguyen Manh', N'Tam', '1986-02-27', N'Nam', N'Kien Giang', N'K11'),
+(N'K1109', N'Phan Thi Thanh', N'Tam', '1986-01-27', N'Nu', N'Vinh Long', N'K11'),
+(N'K1110', N'Le Hoai', N'Thuong', '1986-02-05', N'Nu', N'Can Tho', N'K11'),
+(N'K1111', N'Le Ha', N'Vinh', '1986-12-25', N'Nam', N'Vinh Long', N'K11'),
+(N'K1201', N'Nguyen Van', N'B', '1986-02-11', N'Nam', N'TpHCM', N'K12'),
+(N'K1202', N'Nguyen Thi Kim', N'Duyen', '1986-01-18', N'Nu', N'TpHCM', N'K12'),
+(N'K1203', N'Tran Thi Kim', N'Duyen', '1986-09-17', N'Nu', N'TpHCM', N'K12'),
+(N'K1204', N'Truong My', N'Hanh', '1986-05-19', N'Nu', N'Dong Nai', N'K12'),
+(N'K1205', N'Nguyen Thanh', N'Nam', '1986-04-17', N'Nam', N'TpHCM', N'K12'),
+(N'K1206', N'Nguyen Thi Truc', N'Thanh', '1986-03-04', N'Nu', N'Kien Giang', N'K12'),
+(N'K1207', N'Tran Thi Bich', N'Thuy', '1986-02-08', N'Nu', N'Nghe An', N'K12'),
+(N'K1208', N'Huynh Thi Kim', N'Trieu', '1986-04-08', N'Nu', N'Tay Ninh', N'K12'),
+(N'K1209', N'Pham Thanh', N'Trieu', '1986-02-23', N'Nam', N'TpHCM', N'K12'),
+(N'K1210', N'Ngo Thanh', N'Tuan', '1986-02-14', N'Nam', N'TpHCM', N'K12'),
+(N'K1211', N'Do Thi', N'Xuan', '1986-03-09', N'Nu', N'Ha Noi', N'K12'),
+(N'K1212', N'Le Thi Phi', N'Yen', '1986-03-12', N'Nu', N'TpHCM', N'K12'),
+(N'K1301', N'Nguyen Thi Kim', N'Cuc', '1986-06-09', N'Nu', N'Kien Giang', N'K13'),
+(N'K1302', N'Truong Thi My', N'Hien', '1986-03-18', N'Nu', N'Nghe An', N'K13'),
+(N'K1303', N'Le Duc', N'Hien', '1986-03-21', N'Nam', N'Tay Ninh', N'K13'),
+(N'K1304', N'Le Quang', N'Hien', '1986-04-18', N'Nam', N'TpHCM', N'K13'),
+(N'K1305', N'Le Thi', N'Huong', '1986-03-27', N'Nu', N'TpHCM', N'K13'),
+(N'K1306', N'Nguyen Thai', N'Huu', '1986-03-30', N'Nam', N'Ha Noi', N'K13'),
+(N'K1307', N'Tran Minh', N'Man', '1986-05-28', N'Nam', N'TpHCM', N'K13'),
+(N'K1308', N'Nguyen Hieu', N'Nghia', '1986-04-08', N'Nam', N'Kien Giang', N'K13'),
+(N'K1309', N'Nguyen Trung', N'Nghia', '1987-01-18', N'Nam', N'Nghe An', N'K13'),
+(N'K1310', N'Tran Thi Hong', N'Tham', '1986-04-22', N'Nu', N'Tay Ninh', N'K13'),
+(N'K1311', N'Tran Minh', N'Thuc', '1986-04-04', N'Nam', N'TpHCM', N'K13'),
+(N'K1312', N'Nguyen Thi Kim', N'Yen', '1986-09-07', N'Nu', N'TpHCM', N'K13');
+
+INSERT GIANGDAY (MALOP, MAMH, MAGV, HOCKY, NAM, TUNGAY, DENNGAY) VALUES
+(N'K11', N'THDC', N'GV07', 1, 2006, '2006-01-02', '2006-05-12'),
+(N'K12', N'THDC', N'GV06', 1, 2006, '2006-01-02', '2006-05-12'),
+(N'K13', N'THDC', N'GV15', 1, 2006, '2006-01-02', '2006-05-12'),
+(N'K11', 'CTRR', N'GV02', 1, 2006, '2006-01-09', '2006-05-17'),
+(N'K12', N'CTRR', N'GV02', 1, 2006, '2006-01-09', '2006-05-17'),
+(N'K13', N'CTRR', N'GV08', 1, 2006, '2006-01-09', '2006-05-17'),
+(N'K11', N'CSDL', N'GV05', 2, 2006, '2006-06-01', '2006-07-15'),
+(N'K12', N'CSDL', N'GV09', 2, 2006, '2006-06-01', '2006-07-15'),
+(N'K13', N'CTDLGT', N'GV15', 2, 2006, '2006-06-01', '2006-07-15'),
+(N'K13', N'CSDL', N'GV05', 3, 2006, '2006-08-01', '2006-12-15'),
+(N'K13', N'DHMT', N'GV07', 3, 2006, '2006-08-01', '2006-12-15'),
+(N'K11', N'CTDLGT', N'GV15', 3, 2006, '2006-08-01', '2006-12-15'),
+(N'K12', N'CTDLGT', N'GV15', 3, 2006, '2006-08-01', '2006-12-15'),
+(N'K11', N'HDH', N'GV04', 1, 2007, '2007-01-02', '2007-02-18'),
+(N'K12', N'HDH', N'GV04', 1, 2007, '2007-01-02', '2007-03-20'),
+(N'K11', N'DHMT', N'GV07', 1, 2007, '2007-02-18', '2007-03-20');
+
+INSERT KETQUATHI (MAHV, MAMH, LANTHI, NGTHI, DIEM, KQUA) VALUES
+(N'K1101', N'CSDL', 1, '2006-07-20', 10, N'Dat'),
+(N'K1101', N'CTDLGT', 1, '2006-12-28', 9, N'Dat'),
+(N'K1101', N'THDC', 1, '2006-05-20', 9, N'Dat'),
+(N'K1101', N'CTRR', 1, '2006-05-13', 9.5, N'Dat'),
+(N'K1102', N'CSDL', 1, '2006-07-20', 4, N'Khong Dat'),
+(N'K1102', N'CSDL', 2, '2006-07-27', 4.25, N'Khong Dat'),
+(N'K1102', N'CSDL', 3, '2006-08-10', 4.5, N'Khong Dat'),
+(N'K1102', N'CTDLGT', 1, '2006-12-28', 4.5, N'Khong Dat'),
+(N'K1102', N'CTDLGT', 2, '2007-01-05', 4, N'Khong Dat'),
+(N'K1102', N'CTDLGT', 3, '2007-01-15', 6, N'Dat'),
+(N'K1102', N'THDC', 1, '2006-05-20', 5, N'Dat'),
+(N'K1102', N'CTRR', 1, '2006-05-13', 7, N'Dat'),
+(N'K1103', N'CSDL', 1, '2006-07-20', 3.5, N'Khong Dat'),
+(N'K1103', N'CSDL', 2, '2006-07-27', 8.25, N'Dat'),
+(N'K1103', N'CTDLGT', 1, '2006-12-28', 7, N'Dat'),
+(N'K1103', N'THDC', 1, '2006-05-20', 8, N'Dat'),
+(N'K1103', N'CTRR', 1, '2006-05-13', 6.5, N'Dat'),
+(N'K1104', N'CSDL', 1, '2006-07-20', 3.75, N'Khong Dat'),
+(N'K1104', N'CTDLGT', 1, '2006-12-28', 4, N'Khong Dat'),
+(N'K1104', N'THDC', 1, '2006-05-20', 4, N'Khong Dat'),
+(N'K1104', N'CTRR', 1, '2006-05-13', 4, N'Khong Dat'),
+(N'K1104', N'CTRR', 2, '2006-05-20', 3.5, N'Khong Dat'),
+(N'K1104', N'CTRR', 3, '2006-06-30', 4, N'Khong Dat'),
+(N'K1201', N'CSDL', 1, '2006-07-20', 6, N'Dat'),
+(N'K1201', N'CTDLGT', 1, '2006-12-28', 5, N'Dat'),
+(N'K1201', N'THDC', 1, '2006-05-20', 8.5, N'Dat'),
+(N'K1201', N'CTRR', 1, '2006-05-13', 9, N'Dat'),
+(N'K1202', N'CSDL', 1, '2006-07-20', 8, N'Dat'),
+(N'K1202', N'CTDLGT', 1, '2006-12-28', 4, N'Khong Dat'),
+(N'K1202', N'CTDLGT', 2, '2007-01-05', 5, N'Dat'),
+(N'K1202', N'THDC', 1, '2006-05-20', 4, N'Khong Dat'),
+(N'K1202', N'THDC', 2, '2006-05-27', 4, N'Khong Dat'),
+(N'K1202', N'CTRR', 1, '2006-05-13', 3, N'Khong Dat'),
+(N'K1202', N'CTRR', 2, '2006-05-20', 4, N'Khong Dat'),
+(N'K1202', N'CTRR', 3, '2006-06-30', 6.25, N'Dat'),
+(N'K1203', N'CSDL', 1, '2006-07-20', 9.25, N'Dat'),
+(N'K1203', N'CTDLGT', 1, '2006-12-28', 9.5, N'Dat'),
+(N'K1203', N'THDC', 1, '2006-05-20', 10, N'Dat'),
+(N'K1203', N'CTRR', 1, '2006-05-13', 10, N'Dat'),
+(N'K1204', N'CSDL', 1, '2006-07-20', 8.5, N'Dat'),
+(N'K1204', N'CTDLGT', 1, '2006-12-28', 6.75, N'Dat'),
+(N'K1204', N'THDC', 1, '2006-05-20', 4, N'Khong Dat'),
+(N'K1204', N'CTRR', 1, '2006-05-13', 6, N'Dat'),
+(N'K1301', N'CSDL', 1, '2006-12-20', 4.25, N'Khong Dat'),
+(N'K1301', N'CTDLGT', 1, '2006-07-25', 8, N'Dat'),
+(N'K1301', N'THDC', 1, '2006-05-20', 7.75, N'Dat'),
+(N'K1301', N'CTRR', 1, '2006-05-13', 8, N'Dat'),
+(N'K1302', N'CSDL', 1, '2006-12-20', 6.75, N'Dat'),
+(N'K1302', N'CTDLGT', 1, '2006-07-25', 5, N'Dat'),
+(N'K1302', N'THDC', 1, '2006-05-20', 8, N'Dat'),
+(N'K1302', N'CTRR', 1, '2006-05-13', 8.5, N'Dat'),
+(N'K1303', N'CSDL', 1, '2006-12-20', 4, N'Khong Dat'),
+(N'K1303', N'CTDLGT', 1, '2006-07-25', 4.5, N'Khong Dat'),
+(N'K1303', N'CTDLGT', 2, '2006-08-07', 4, N'Khong Dat'),
+(N'K1303', N'CTDLGT', 3, '2006-08-15', 4.25, N'Khong Dat'),
+(N'K1303', N'THDC', 1, '2006-05-20', 4.5, N'Khong Dat'),
+(N'K1303', N'CTRR', 1, '2006-05-13', 3.25, N'Khong Dat'),
+(N'K1303', N'CTRR', 2, '2006-05-20', 5, N'Dat'),
+(N'K1304', N'CSDL', 1, '2006-12-20', 7.75, N'Dat'),
+(N'K1304', N'CTDLGT', 1, '2006-07-25', 9.75, N'Dat'),
+(N'K1304', N'THDC', 1, '2006-05-20', 5.5, N'Dat'),
+(N'K1304', N'CTRR', 1, '2006-05-13', 5, N'Dat'),
+(N'K1305', N'CSDL', 1, '2006-12-20', 9.25, N'Dat'),
+(N'K1305', N'CTDLGT', 1, '2006-07-25', 10, N'Dat'),
+(N'K1305', N'THDC', 1, '2006-05-20', 8, N'Dat'),
+(N'K1305', N'CTRR', 1, '2006-05-13', 10, N'Dat');
+
+UPDATE KHOA SET TRGKHOA = N'GV01' 
+WHERE MAKHOA = N'KHMT'
+
+UPDATE KHOA SET TRGKHOA = N'GV02' 
+WHERE MAKHOA = N'HTTT'
+
+UPDATE KHOA SET TRGKHOA = N'GV03' 
+WHERE MAKHOA = N'CNPM'
+
+UPDATE KHOA SET TRGKHOA = N'GV04' 
+WHERE MAKHOA = N'MTT'
+
+UPDATE KHOA SET TRGKHOA = N'Null'
+WHERE MAKHOA = N'KTMT'
+
+-- 11. Học viên ít nhất là 18 tuổi.
+ALTER TABLE HOCVIEN ADD
+CONSTRAINT CK_TUOI CHECK (GETDATE() - NGSINH >= 18)
+
+-- 12. Giảng dạy một môn học ngày bắt đầu (TUNGAY) phải nhỏ hơn ngày kết thúc (DENNGAY).
+ALTER TABLE GIANGDAY ADD
+CONSTRAINT CK_NGAY CHECK (TUNGAY < DENNGAY)
+
+-- 13. Giáo viên khi vào làm ít nhất là 22 tuổi.
+ALTER TABLE GIAOVIEN ADD
+CONSTRAINT CK_TUOIGV CHECK (NGVL - NGSINH >= 22)
+
+-- 14. Tất cả các môn học đều có số tín chỉ lý thuyết và tín chỉ thực hành chênh lệch nhau không quá 3.
+ALTER TABLE MONHOC 
+ADD CONSTRAINT CK_LT_TH CHECK (ABS(TCLT - TCTH) <= 3)
+
+
+
+-- III. 
+-- 1. In ra danh sách (mã học viên, họ tên, ngày sinh, mã lớp) lớp trưởng của các lớp.
+SELECT MAHV, HO, TEN, NGSINH, LOP.MALOP FROM HOCVIEN, LOP
+WHERE LOP.TRGLOP = HOCVIEN.MAHV
+
+-- 2. In ra bảng điểm khi thi (mã học viên, họ tên, lần thi, điểm số) môn CTRR của lớp “K12”, sắp xếp theo tên, họ học viên.
+SELECT HOCVIEN.MAHV, HO, TEN, LANTHI, DIEM FROM HOCVIEN, KETQUATHI, MONHOC
+WHERE MONHOC.MAMH = KETQUATHI.MAMH AND KETQUATHI.MAHV = HOCVIEN.MAHV AND TENMH = N'Cau truc roi rac' AND MALOP = N'K12'
+ORDER BY TEN ASC, HO ASC
+
+-- 3. In ra danh sách những học viên (mã học viên, họ tên) và những môn học mà học viên đó thi lần thứ nhất đã đạt.
+SELECT HOCVIEN.MAHV, HO, TEN, MONHOC.TENMH FROM HOCVIEN, KETQUATHI, MONHOC
+WHERE KETQUATHI.MAHV = HOCVIEN.MAHV AND KETQUATHI.MAMH = MONHOC.MAMH AND LANTHI = 1 AND KQUA = N'Dat'
+
+-- 4. In ra danh sách học viên (mã học viên, họ tên) của lớp “K11” thi môn CTRR không đạt (ở lần thi 1).
+SELECT HOCVIEN.MAHV, HO, TEN FROM HOCVIEN, KETQUATHI, MONHOC
+WHERE KETQUATHI.MAHV = HOCVIEN.MAHV AND KETQUATHI.MAMH = MONHOC.MAMH AND LANTHI = 1 AND KQUA = N'Khong Dat' AND MALOP = N'K11' AND TENMH = N'Cau truc roi rac'
+
+-- 5. Danh sách học viên (mã học viên, họ tên) của lớp “K” thi môn CTRR không đạt (ở tất cả các lần thi).
+SELECT HOCVIEN.MAHV, HO, TEN FROM HOCVIEN, KETQUATHI, MONHOC
+WHERE KETQUATHI.MAHV = HOCVIEN.MAHV AND KETQUATHI.MAMH = MONHOC.MAMH AND KQUA = N'Khong Dat' AND MALOP LIKE N'K%' AND TENMH = N'Cau truc roi rac'
+
